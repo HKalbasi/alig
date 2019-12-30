225f5b485c23a938431a852664b708c879666cd6
HKalbasi <kalbasi@ce.sharif.edu>
2019-12-30 14:18:27 +0330

    refactor(front): move tab part to another component

--- front/header/header-root.js
+++ front/header/header-root.js
@@ -1,5 +1,4 @@
-const template = 
-/*html*/`<div class="header-wrapper">
+const template = `<div class="header-wrapper">
 
 	<div class="ui huge breadcrumb repo-title">
 
@@ -63,46 +62,12 @@ const template =
 		</div>
 
 	</div>
-	<div class="ui tabular stackable menu navbar" style="border-bottom:none">
-
-		<router-link :to="'/'+branch+'/file/'" :class="tabCss('file')">
-			Code
-		</router-link>
-
-
-		<router-link :to="'/'+branch+'/issues/'" :class="tabCss('issues')">
-			Issues / Merge requests <span class="ui blue small label">13</span>
-		</router-link>
-
-
-		<a :class="tabCss('release')" href="/gitea/go-sdk/releases">
-			<i class="octicon octicon-tag"></i> Releases <span class="ui gray small label">0</span>
-		</a>
-
-		<a :class="tabCss('wiki')" href="/gitea/go-sdk/wiki">
-			<i class="octicon octicon-book"></i> Wiki
-		</a>
-
-		<a :class="tabCss('activity')" href="/gitea/go-sdk/activity">
-			<i class="octicon octicon-pulse"></i> Activity
-		</a>
-
-
-
-
-
-	</div>
-	<div class="ui tabs divider" style="margin-top:0; margin-bottom:20px;"></div>
+	
 </div>`;
 
 const props = ['projectName', 'branch', 'selectedTab'];
 
 export const HeaderRoot = {
   template,
-  methods: {
-    tabCss: function(id) {
-      return (this.selectedTab === id ? 'active ' : '') + 'item';
-    },
-  },
   props,
 };
---
+++ front/header/header-tabs.js
@@ -0,0 +1,40 @@
+export const HeaderTabs = {
+  props: ['selectedTab'],
+  methods: {
+    tabCss: function(id) {
+      return (this.selectedTab === id ? 'active ' : '') + 'item';
+    },
+  },
+  template: `
+  <div class="ui tabular stackable menu navbar" style="border-bottom:none">
+
+		<router-link :to="'/'+branch+'/file/'" :class="tabCss('file')">
+			Code
+		</router-link>
+
+
+		<router-link :to="'/'+branch+'/issues/'" :class="tabCss('issues')">
+			Issues / Merge requests <span class="ui blue small label">13</span>
+		</router-link>
+
+
+		<a :class="tabCss('release')" href="/gitea/go-sdk/releases">
+			<i class="octicon octicon-tag"></i> Releases <span class="ui gray small label">0</span>
+		</a>
+
+		<a :class="tabCss('wiki')" href="/gitea/go-sdk/wiki">
+			<i class="octicon octicon-book"></i> Wiki
+		</a>
+
+		<a :class="tabCss('activity')" href="/gitea/go-sdk/activity">
+			<i class="octicon octicon-pulse"></i> Activity
+		</a>
+
+
+
+
+
+	</div>
+	<div class="ui tabs divider" style="margin-top:0; margin-bottom:20px;"></div>
+  `,
+}
