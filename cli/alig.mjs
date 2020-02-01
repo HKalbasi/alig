10ffa47eaff91cc9387130aa35a711776479176d
HKalbasi <kalbasi@ce.sharif.edu>
2020-02-01 11:47:50 +0330

    feat: add --id option

--- cli/alig.mjs
+++ cli/alig.mjs
@@ -2,7 +2,7 @@ import commander from "commander";
 import { readIssue, addComment } from "../alig/issue/main.mjs";
 import { git } from "../gitEngine/wrapper.mjs";
 import readLine from "readline";
-
+import { issueCliBuilder } from "../alig/issue/cli.mjs";
 
 const main = async () => {
   const rl = readLine.createInterface({
@@ -14,16 +14,19 @@ const main = async () => {
       rl.question(q,res);
     });
   };
+  const { viewIssue } = issueCliBuilder(rl);
   const program = new commander.Command();
   program
     .command('issue')
     .description('show, create or modify issues')
     .option('-n, --new', 'open a new issue')
+    .option('-i, --id <id>', 'show issue with specific id')
     .action(async (opt) => {
       if (opt.new) {
         await question('Enter title: ');
       }
       else {
+        await viewIssue(opt.id);
         process.exit(0);
       }
     });
