baec2a5b81fb7e1c3785e346946780f64c099212
HKalbasi <kalbasi@ce.sharif.edu>
2020-02-02 00:26:35 +0330

    refactor: move import.meta to seperate file

--- index.mjs
+++ index.mjs
@@ -1,21 +1,20 @@
+/* eslint-disable immutable/no-mutation */
 import http from "http";
 import { restHandlerBuilder } from "./rest/index.mjs";
-import koa from "koa";
+import Koa from "koa";
 import koaSend from "koa-send";
 import cgi from "cgi";
 import path from "path";
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+import { projectRoot } from "./util/rootAddress.mjs";
 
 const repoPath = path.resolve(process.argv[2]);
 const restHandler = restHandlerBuilder('', repoPath);
 
-const frontHandler = (new koa()).use(async (ctx) => {
-  if (ctx.path.substr(0,6) == '/dist/') {
-    await koaSend(ctx, 'front/dist/'+ctx.path.substr(6), { root: __dirname });
-  }
-  else {
-    await koaSend(ctx, 'front/index.html', { root: __dirname });
+const frontHandler = (new Koa()).use(async (ctx) => {
+  if (ctx.path.substr(0, 6) === '/dist/') {
+    await koaSend(ctx, 'front/dist/' + ctx.path.substr(6), { root: projectRoot });
+  } else {
+    await koaSend(ctx, 'front/index.html', { root: projectRoot });
   }
 }).callback();
 
@@ -29,25 +28,24 @@ const gitSmartHttp = cgi('git', {
 
 const main = async () => {
   const server = http.createServer((req, res) => {
-    if (req.url.substring(0,5) == '/rest'){
+    if (req.url.substring(0, 5) === '/rest') {
       restHandler(req, res);
-    }
-    else if (req.url.substring(0,9) === '/alig.git') {
-      req.url = req.url.slice(9);
+    } else if (req.url.substring(0, 9) === '/alig.git') {
+      const url = req.url.slice(9);
       console.log(req.url, req.headers);
-      if (req.url === '/info/refs?service=git-receive-pack') {
-        if (req.headers['authorization'] !== 'Basic aGFtaWQ6cmV6YQ==') {
+      if (url === '/info/refs?service=git-receive-pack') {
+        if (req.headers.authorization !== 'Basic aGFtaWQ6cmV6YQ==') {
           res.statusCode = 401;
           res.setHeader('WWW-Authenticate', 'Basic');
           res.end();
-          return;  
+          return;
         }
       }
-      gitSmartHttp(req,res);
-    }
-    else frontHandler(req,res);
+      gitSmartHttp(req, res);
+    } else frontHandler(req, res);
   });
   server.listen(8080);
 };
 
+// eslint-disable-next-line toplevel/no-toplevel-side-effect
 main();
