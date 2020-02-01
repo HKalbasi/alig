018deb7255fc6433b1d795ba8cc6e2766b6e80c8
HKalbasi <kalbasi@ce.sharif.edu>
2020-02-01 11:38:12 +0330

    refactor: move issue cli functions to seperate file

---
+++ alig/issue/cli.mjs
@@ -0,0 +1,80 @@
+import { readIssue } from "./main.mjs";
+import { promisify } from "util";
+import editor from "editor";
+import fs from "fs";
+import { readConfig } from "../gitEngine/globalConfig.mjs";
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+const authorToString = ({name, email}) => `${name}<${email}>`;
+
+const multilineInput = async (init) => {
+  const adr = `/tmp/lanate khoda`;
+  await writeFile(adr, init);
+  await new Promise((res) => {
+    editor(adr, ()=>{
+      res();
+    });
+  });
+  return (await readFile(adr)).toString();
+};
+
+export const issueCliBuilder = (rl) => {
+  const question = (q) => {
+    return new Promise((res) => {
+      rl.question(q,res);
+    });
+  };
+  const questionWithDefault = async (q, d) => {
+    const r = await question(`${q}: (${d}) `);
+    if (r === '') return d;
+    return r;
+  };
+  const addCommentCli = async (id) => {
+    const author = {
+      name: await questionWithDefault(
+        'Enter your name',
+        await readConfig("user.name", '.'),
+      ),
+      email: await questionWithDefault(
+        'Enter your email',
+        await readConfig("user.email", '.'),
+      ),
+    };
+    rl.pause();
+    const text = await multilineInput('Now enter issue text in markdown here, then save and close.');
+    rl.resume();
+    await addComment({
+      text,
+      author,
+      id,
+    });
+  };
+  const viewIssue = async (id) => {
+    try{
+      const issue = await readIssue(id);
+      console.log(issue.head.title);
+      console.log(`by ${authorToString(issue.head.author.name)} at ${issue.head.time}`);  
+      issue.body.forEach(e => {
+        if (e.type === 'comment') {
+          console.log(`${authorToString(e.author)} commented at ${e.time}:`);
+          console.log(e.text);
+        }
+        else {
+          console.log(e);
+        }
+      });
+      const res = await question('a for add comment, and anything else for exit:\n');
+      if (res === 'a') {
+        await addCommentCli(id);
+      }
+    }
+    catch(e) {
+      console.log(e);
+    }
+  };
+  return {
+    viewIssue,
+  }
+};
--- cli/alig.mjs
+++ cli/alig.mjs
@@ -1,27 +1,8 @@
 import commander from "commander";
-import { readIssue, addComment } from "../alig/issue.mjs";
+import { readIssue, addComment } from "../alig/issue/main.mjs";
 import { git } from "../gitEngine/wrapper.mjs";
 import readLine from "readline";
-import { promisify } from "util";
-import editor from "editor";
-import fs from "fs";
-import { readConfig } from "../gitEngine/globalConfig.mjs";
 
-const readFile = promisify(fs.readFile);
-const writeFile = promisify(fs.writeFile);
-
-const authorToString = ({name, email}) => `${name}<${email}>`;
-
-const multilineInput = async (init) => {
-  const adr = `/tmp/lanate khoda`;
-  await writeFile(adr, init);
-  await new Promise((res) => {
-    editor(adr, ()=>{
-      res();
-    });
-  });
-  return (await readFile(adr)).toString();
-};
 
 const main = async () => {
   const rl = readLine.createInterface({
@@ -33,60 +14,17 @@ const main = async () => {
       rl.question(q,res);
     });
   };
-  const questionWithDefault = async (q, d) => {
-    const r = await question(`${q}: (${d}) `);
-    if (r === '') return d;
-    return r;
-  }
-  const addIssueCli = async (id) => {
-    const author = {
-      name: await questionWithDefault(
-        'Enter your name',
-        await readConfig("user.name", '.'),
-      ),
-      email: await questionWithDefault(
-        'Enter your email',
-        await readConfig("user.email", '.'),
-      ),
-    };
-    rl.pause();
-    const text = await multilineInput('Now enter text here, then save and close.');
-    rl.resume();
-    await addComment({
-      text,
-      author,
-      id,
-    });
-  };
   const program = new commander.Command();
   program
-    .command('issue <id>')
-    .description('show issue with id <id>')
-    .action(async (id) => {
-      try{
-        const issue = await readIssue(id);
-        console.log(issue.head.title);
-        console.log(`by ${authorToString(issue.head.author.name)} at ${issue.head.time}`);  
-        issue.body.forEach(e => {
-          if (e.type === 'comment') {
-            console.log(`${authorToString(e.author)} commented at ${e.time}:`);
-            console.log(e.text);
-          }
-          else {
-            console.log(e);
-          }
-        });
-        const res = await question('a for add comment, and anything else for exit:\n');
-        if (res === 'a') {
-          await addIssueCli(id);
-          process.exit(0);
-        }
-        else {
-          process.exit(0);
-        }
+    .command('issue')
+    .description('show, create or modify issues')
+    .option('-n, --new', 'open a new issue')
+    .action(async (opt) => {
+      if (opt.new) {
+        await question('Enter title: ');
       }
-      catch(e) {
-        console.log(e);
+      else {
+        process.exit(0);
       }
     });
   program.parse(process.argv);
