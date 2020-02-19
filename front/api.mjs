9fd04162668a50b8a7cc8e18789b02500e71900c
HKalbasi <kalbasi@ce.sharif.edu>
2020-02-19 20:56:20 +0330

    feat: add body param

--- front/api.mjs
+++ front/api.mjs
@@ -1,12 +1,18 @@
-export const getFromApi = async (x) => {
+export const getFromApi = async (x, body = {}) => {
   const jwt = localStorage.getItem('jwt');
-  const res = jwt === undefined
-    ? await fetch(`/rest/${x}`)
-    : await fetch(`/rest/${x}`, {
-      headers: {
-        Authorization: `Bearer ${jwt}`,
-      },
-    });
+  const headersWithoutAuth = {
+    'Content-Type': 'application/json',
+  };
+  const headers = jwt === undefined ? headersWithoutAuth : {
+    ...headersWithoutAuth,
+    Authorization: `Bearer ${jwt}`,
+  };
+  const params = {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(body),
+  };
+  const res = await fetch(`/rest/${x}`, params);
   if (res.status !== 200) throw new Error(`fetch(/rest/${x}) failed: ${res.status}`);
   return res.json();
 };
